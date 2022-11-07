e8b36086b6a10c4a700bdbdac04c863834f6b142
sorhawell <sorhawell@gmail.com>
2022-11-07 11:44:34 +0100

    add minimal polars example

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,21 @@
 use extendr_api::prelude::*;
+use polars::prelude::*;
 
 /// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
 fn hello_world() -> &'static str {
     "Hello world!"
+}
+
+
+
+#[extendr]
+fn hello_polars() {
+    let s0 = Series::new("days", [0, 1, 2].as_ref());
+    let s1 = Series::new("temp", [22.1, 19.9, 7.].as_ref());
+    let df = DataFrame::new(vec![s0, s1]).unwrap();
+    rprintln!("hello polars {}",df);
 }
 
 // Macro to generate exports.
